6229f3bd7b40d33adf014d272261d79ec8c3382d
Jason Bell <ja50n8@yahoo.co.uk>
2017-06-22 10:03:10 +0100

    Social App
    Styling

---
+++ social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  social
+//
+//  Created by Jason Bell on 22/06/2017.
+//  Copyright © 2017 Cold Entertainment. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0

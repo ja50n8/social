20c158c12f843b138a638c743ccb5d8d32302866
Jason Bell <ja50n8@yahoo.co.uk>
2017-06-23 15:44:45 +0100

    Social
    FeedVC UI setup

--- social/CircleView.swift
+++ social/CircleView.swift
@@ -1,23 +1,27 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  social
 //
-//  Created by Jason Bell on 22/06/2017.
+//  Created by Jason Bell on 23/06/2017.
 //  Copyright © 2017 Cold Entertainment. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
-
+class CircleView: UIImageView {
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
+        
+    }
+
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        layer.cornerRadius = self.frame.width / 2
     }
 
 }

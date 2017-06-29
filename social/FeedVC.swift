d3a1642b145349538635602e0fcac0358c027c74
Jason Bell <ja50n8@yahoo.co.uk>
2017-06-29 23:34:24 +0100

    Social
    Likes

--- social/FeedVC.swift
+++ social/FeedVC.swift
@@ -15,8 +15,7 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIIm
     @IBOutlet weak var tableView: UITableView!    
     @IBOutlet weak var imageAdd: CircleView!
     @IBOutlet weak var captionField: FancyField!
-    
-    
+        
     var posts = [Post]()
     var imagePicker: UIImagePickerController!
     static var imageCache: NSCache<NSString, UIImage> = NSCache()
@@ -67,12 +66,10 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIIm
             
             if let img = FeedVC.imageCache.object(forKey: post.imageUrl as NSString) {
                 cell.configureCell(post: post, img: img)
-                return cell
             } else {
                 cell.configureCell(post: post)
-                return cell
             }
-  
+            return cell
         } else {
             return PostCell()
         }
--- social/Post.swift
+++ social/Post.swift
@@ -7,13 +7,15 @@
 //
 
 import Foundation
+import Firebase
 
 class Post {
     
     private var _caption: String!
     private var _imageUrl: String!
     private var _likes: Int!
-    private var _poksKey: String!
+    private var _postKey: String!
+    private var _postRef: DatabaseReference!
     
     var caption: String {
         return _caption
@@ -29,7 +31,7 @@ class Post {
     }
     
     var postKey: String {
-        return _poksKey
+        return _postKey
     }
     
     init(caption: String, imageUrl: String, likes: Int) {
@@ -39,7 +41,7 @@ class Post {
     }
     
     init(postKey: String, postData: Dictionary<String, AnyObject>) {
-        self._poksKey = postKey
+        self._postKey = postKey
         
         if let caption = postData["caption"] as? String {
             self._caption = caption
@@ -52,6 +54,18 @@ class Post {
         if let likes = postData["likes"] as? Int? {
             self._likes = likes
         }
+        
+        _postRef = DataService.ds.REF_POSTS.child(_postKey)
+    }
+    
+    func adjustLikes(addLike: Bool) {
+        if addLike {
+            _likes = _likes + 1
+        } else {
+            _likes = _likes - 1
+        }
+        _postRef.child("likes").setValue(_likes)
+        
     }
     
 }
